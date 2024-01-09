08361744360c68ae3fe5c84fe30ee3680a0eae09
barrettxavier <barrettxavier@yahoo.com>
2024-01-09 13:00:14 -0500

    initial commit, be sure to run npm i

---
+++ src/client/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <section className="w-screen">
+      <div className="flex justify-center items-center">
+        <h1>2D-Coders</h1>
+      </div>
+    </section>
+  );
+}
+
+export default App;
